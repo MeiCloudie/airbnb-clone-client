ac0d9c20f13e7bb260162644931e46fa08c26552
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-09-12 20:18:42 +0700

    feat(api): Install and Setup Axios | Create local storage utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,30 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
+// Utility function to merge Tailwind classes
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+// Utility functions to interact with localStorage
+
+// Đặt giá trị vào localStorage với kiểu dữ liệu an toàn
+export function setLocalStorage<T>(key: string, value: T): void {
+  try {
+    const stringJson = JSON.stringify(value) // Chuyển đổi thành chuỗi JSON
+    localStorage.setItem(key, stringJson)
+  } catch (error) {
+    console.error('Error setting localStorage:', error)
+  }
+}
+
+// Lấy giá trị từ localStorage và trả về đúng kiểu dữ liệu
+export function getLocalStorage<T>(key: string): T | null {
+  try {
+    const dataLocal = localStorage.getItem(key) // Lấy dữ liệu từ localStorage
+    return dataLocal ? (JSON.parse(dataLocal) as T) : null // Parse dữ liệu nếu không null
+  } catch (error) {
+    console.error('Error getting localStorage:', error)
+    return null
+  }
 }
