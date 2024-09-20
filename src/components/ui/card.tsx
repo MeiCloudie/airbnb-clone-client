55f25e724f2e9ff1b8da5b017a449553e0387fa2
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-09-20 20:47:08 +0700

    feat(home): Create Rooms List UI - demo dummy data

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -7,6 +7,11 @@ const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElemen
 ))
 Card.displayName = 'Card'
 
+const CardImage = React.forwardRef<HTMLImageElement, React.ImgHTMLAttributes<HTMLImageElement>>(
+  ({ className, ...props }, ref) => <img ref={ref} className={cn('h-auto w-full rounded-t-xl', className)} {...props} />
+)
+CardImage.displayName = 'CardImage'
+
 const CardHeader = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
   ({ className, ...props }, ref) => (
     <div ref={ref} className={cn('flex flex-col space-y-1.5 p-6', className)} {...props} />
@@ -38,4 +43,4 @@ const CardFooter = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDiv
 )
 CardFooter.displayName = 'CardFooter'
 
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
+export { Card, CardImage, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
