96456d1f7142858a5ade7780e3ba947455ee0a23
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-09-25 15:16:16 +0700

    feat(user-header): Create Recent Search Locations (localstorage method)

--- src/components/dialog/search-dialog.tsx
+++ src/components/dialog/search-dialog.tsx
@@ -10,13 +10,14 @@ import {
 } from '@/components/ui/dialog'
 import { Separator } from '@/components/ui/separator'
 
-import { Check, ChevronsUpDown } from 'lucide-react'
+import { Check, ChevronsUpDown, Clock } from 'lucide-react'
 
 import { cn } from '@/lib/utils'
 import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from '@/components/ui/command'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 import { useLocation } from '@/hooks/useLocation'
 import { Location } from '@/types/location.type'
+import localStorageService from '@/services/localStorage.service'
 
 interface SearchDialogProps {
   open: boolean
@@ -29,12 +30,17 @@ const SearchDialog: React.FC<SearchDialogProps> = ({ open, onOpenChange, onClose
   const [openCombobox, setOpenCombobox] = React.useState(false)
   const [selectedLocation, setSelectedLocation] = useState<{ id: number; label: string } | null>(null)
   const [searchTerm, setSearchTerm] = useState('')
+  const [recentSearches, setRecentSearches] = useState<{ id: number; label: string }[]>([])
 
   const { dataAllLocations, getAllLocations, isLoading } = useLocation()
 
   useEffect(() => {
     if (open) {
       getAllLocations()
+
+      // Lấy lại dữ liệu recent searches từ localStorage
+      const recent = localStorageService.get<{ id: number; label: string }[], []>('recentSearches', [])
+      setRecentSearches(recent)
     }
   }, [open, getAllLocations])
 
@@ -47,13 +53,31 @@ const SearchDialog: React.FC<SearchDialogProps> = ({ open, onOpenChange, onClose
   }
 
   const handleSelectLocation = (location: Location) => {
-    console.log('Selected Location:', location)
-    setSelectedLocation({
+    const newSelectedLocation = {
       id: location.id,
       label: `${location.tenViTri}, ${location.tinhThanh}, ${location.quocGia}`
-    })
+    }
+    console.log('Selected Location:', newSelectedLocation)
+
+    setSelectedLocation(newSelectedLocation)
     setOpenCombobox(false)
     setSearchTerm('') // Reset searchTerm để hiển thị lại danh sách đầy đủ
+
+    // Lưu vào localStorage và cập nhật recent searches
+    const updatedSearches = [newSelectedLocation, ...recentSearches.filter((item) => item.id !== location.id)].slice(
+      0,
+      3
+    )
+    localStorageService.set('recentSearches', updatedSearches)
+    setRecentSearches(updatedSearches) // Cập nhật giao diện
+  }
+
+  // Xử lý khi chọn từ lịch sử tìm kiếm
+  const handleSelectFromHistory = (location: { id: number; label: string }) => {
+    console.log('Selected Location:', location)
+
+    setSelectedLocation(location)
+    setOpenCombobox(false)
   }
 
   const steps = [
@@ -62,7 +86,71 @@ const SearchDialog: React.FC<SearchDialogProps> = ({ open, onOpenChange, onClose
       name: 'Địa điểm',
       title: 'Địa điểm bất kỳ',
       description: 'Chọn địa điểm bạn muốn tìm kiếm',
-      content: <div>Step 1: Nội dung tìm kiếm địa điểm</div>
+      content: (
+        <>
+          {/* Combobox for Locations */}
+          <Popover open={openCombobox} onOpenChange={setOpenCombobox}>
+            <PopoverTrigger asChild>
+              <Button variant='outline' role='combobox' aria-expanded={openCombobox} className='w-full justify-between'>
+                {selectedLocation ? selectedLocation.label : 'Chọn địa điểm...'}
+                <ChevronsUpDown className='ml-2 h-4 w-4 shrink-0 opacity-50' />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className='w-full min-w-80 p-0' align='start'>
+              <Command>
+                <CommandInput placeholder='Tìm kiếm địa điểm...' value={searchTerm} onValueChange={setSearchTerm} />
+                <CommandList>
+                  {isLoading ? (
+                    <CommandEmpty>Đang tải...</CommandEmpty>
+                  ) : dataAllLocations?.content.length === 0 ? (
+                    <CommandEmpty>Không có địa điểm nào.</CommandEmpty>
+                  ) : (
+                    <CommandGroup className='w-full'>
+                      {dataAllLocations?.content.map((location) => (
+                        <CommandItem
+                          key={location.id}
+                          value={`${location.tenViTri}, ${location.tinhThanh}, ${location.quocGia}`}
+                          onSelect={() => handleSelectLocation(location)}
+                          className='w-full'
+                        >
+                          <Check
+                            className={cn(
+                              'mr-2 h-4 w-4',
+                              selectedLocation?.id === location.id ? 'opacity-100' : 'opacity-0'
+                            )}
+                          />
+                          {location.tenViTri}, {location.tinhThanh}, {location.quocGia}
+                        </CommandItem>
+                      ))}
+                    </CommandGroup>
+                  )}
+                </CommandList>
+              </Command>
+            </PopoverContent>
+          </Popover>
+
+          {/* Recent Search Locations */}
+          {recentSearches.length > 0 && (
+            <div className='mt-6'>
+              <h3 className='text-md font-semibold mb-3'>Tìm kiếm gần đây</h3>
+              <div className='flex flex-col gap-2'>
+                {recentSearches.map((item, index) => (
+                  <div
+                    key={index}
+                    className='flex items-center gap-3 rounded-md cursor-pointer hover:bg-muted'
+                    onClick={() => handleSelectFromHistory(item)}
+                  >
+                    <div className='p-3 rounded-md bg-muted-foreground/20'>
+                      <Clock className='w-4 h-4' />
+                    </div>
+                    <p className='text-sm'>{item.label}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+        </>
+      )
     },
     {
       id: 2,
@@ -124,47 +212,6 @@ const SearchDialog: React.FC<SearchDialogProps> = ({ open, onOpenChange, onClose
 
         {/* Body Content */}
         <div>{steps[currentStep].content}</div>
-
-        {/* Combobox for Locations */}
-        <Popover open={openCombobox} onOpenChange={setOpenCombobox}>
-          <PopoverTrigger asChild>
-            <Button variant='outline' role='combobox' aria-expanded={openCombobox} className='w-full justify-between'>
-              {selectedLocation ? selectedLocation.label : 'Chọn địa điểm...'}
-              <ChevronsUpDown className='ml-2 h-4 w-4 shrink-0 opacity-50' />
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className='w-full min-w-80 p-0' align='start'>
-            <Command>
-              <CommandInput placeholder='Tìm kiếm địa điểm...' value={searchTerm} onValueChange={setSearchTerm} />
-              <CommandList>
-                {isLoading ? (
-                  <CommandEmpty>Đang tải...</CommandEmpty>
-                ) : dataAllLocations?.content.length === 0 ? (
-                  <CommandEmpty>Không có địa điểm nào.</CommandEmpty>
-                ) : (
-                  <CommandGroup className='w-full'>
-                    {dataAllLocations?.content.map((location) => (
-                      <CommandItem
-                        key={location.id}
-                        value={`${location.tenViTri}, ${location.tinhThanh}, ${location.quocGia}`}
-                        onSelect={() => handleSelectLocation(location)}
-                        className='w-full'
-                      >
-                        <Check
-                          className={cn(
-                            'mr-2 h-4 w-4',
-                            selectedLocation?.id === location.id ? 'opacity-100' : 'opacity-0'
-                          )}
-                        />
-                        {location.tenViTri}, {location.tinhThanh}, {location.quocGia}
-                      </CommandItem>
-                    ))}
-                  </CommandGroup>
-                )}
-              </CommandList>
-            </Command>
-          </PopoverContent>
-        </Popover>
 
         {/* Footer Buttons */}
         <DialogFooter>
