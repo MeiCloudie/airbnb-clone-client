6dac2bab3a3a87cf2feda3d1286b27667b03937b
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-09-27 15:58:55 +0700

    refactor(room): Split component room card for reusing

---
+++ src/app/(user)/rooms/room-card.tsx
@@ -0,0 +1,111 @@
+import React from 'react'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardImage, CardTitle } from '@/components/ui/card'
+import { Heart, Star } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import { convertUSDToVND } from '@/format/currency'
+import { ROUTES } from '@/constants/routes'
+import { Room } from '@/types/room.type'
+import { Location } from '@/types/location.type'
+import { useRouter } from 'next/navigation'
+
+interface RoomCardProps {
+  room: Room
+  location: Location | undefined
+  isInWishlist: boolean
+  rating: number
+  onWishlistToggle: () => void
+}
+
+const RoomCard: React.FC<RoomCardProps> = ({ room, location, isInWishlist, rating, onWishlistToggle }) => {
+  const router = useRouter()
+
+  // Tạo mảng các tiện nghi dựa trên giá trị boolean
+  const amenities = [
+    room.wifi && 'Wifi',
+    room.bep && 'Bếp',
+    room.mayGiat && 'Máy giặt',
+    room.banLa && 'Bàn là',
+    room.tivi && 'Tivi',
+    room.dieuHoa && 'Điều hòa',
+    room.doXe && 'Đỗ xe',
+    room.hoBoi && 'Hồ bơi',
+    room.banUi && 'Bàn ủi'
+  ].filter(Boolean) // Lọc ra các giá trị 'true'
+
+  // Tạo mảng chứa thông tin phòng ngủ, giường, phòng tắm
+  const roomDetails = [
+    room.phongNgu > 0 && `${room.phongNgu} phòng ngủ`,
+    room.giuong > 0 && `${room.giuong} giường`,
+    room.phongTam > 0 && `${room.phongTam} phòng tắm`
+  ].filter(Boolean) // Lọc ra các giá trị hợp lệ
+
+  const handleCardClick = () => {
+    router.push(ROUTES.USER.ROOMS.ROOM_DETAIL(room.id.toString()))
+  }
+
+  return (
+    <Card className='group bg-background border-none shadow-none cursor-pointer hover:overflow-hidden'>
+      <div className='relative overflow-hidden rounded-xl'>
+        <CardImage
+          src={room.hinhAnh}
+          alt={`image-${room.id}`}
+          className='aspect-square object-cover object-center transition-transform duration-300 ease-in-out group-hover:scale-105'
+          onClick={handleCardClick}
+        />
+        {/* Heart Icon Button */}
+        <Button
+          variant='ghost'
+          size='icon'
+          className='absolute top-2 right-2 md:top-3 md:right-3 rounded-full transition-transform duration-300 hover:bg-transparent hover:scale-125'
+          onClick={(e) => {
+            e.preventDefault()
+            onWishlistToggle()
+          }}
+        >
+          <Heart className={`w-5 h-5 md:w-7 md:h-7 text-white ${isInWishlist ? 'fill-primary' : 'fill-black/50'}`} />
+        </Button>
+        {/* Guest Favorite */}
+        {rating >= 4.5 && (
+          <div className='absolute top-4 left-3 rounded-full bg-white px-2 py-1 shadow-md'>
+            <p className='text-[9px] md:text-sm text-black font-semibold'>Được khách yêu thích</p>
+          </div>
+        )}
+      </div>
+      <CardHeader className='px-0 pt-4 pb-1 space-y-1' onClick={handleCardClick}>
+        <CardTitle className='flex justify-between gap-5'>
+          <p className='truncate'>{room.tenPhong}</p>
+          {/* Rating */}
+          <p className='flex items-center'>
+            <Star className='w-4 h-4 me-1 fill-current' />
+            {rating >= 0 && rating <= 1 ? 'Mới' : rating}
+          </p>
+        </CardTitle>
+        <CardDescription className='truncate'>
+          {location ? (
+            <>
+              {location.tenViTri} | {location.tinhThanh} <br />
+              {location.quocGia}
+            </>
+          ) : (
+            <>
+              {'Chưa cập nhật vị trí'} <br /> {'???'}
+            </>
+          )}
+        </CardDescription>
+      </CardHeader>
+      <CardContent className='text-sm px-0 pb-3 text-muted-foreground' onClick={handleCardClick}>
+        <p className='truncate font-semibold'>
+          {roomDetails.length > 0 ? roomDetails.join(' • ') : 'Chưa cập nhật thông tin phòng'}
+        </p>
+        <p className='truncate'>{amenities.length > 0 ? amenities.join(' • ') : 'Chưa cập nhật tiện nghi'}</p>
+      </CardContent>
+      <CardFooter className='px-0' onClick={handleCardClick}>
+        <p>
+          <span className='font-semibold'>₫ {convertUSDToVND(room.giaTien)}</span> / đêm
+        </p>
+      </CardFooter>
+    </Card>
+  )
+}
+
+export default RoomCard
--- src/app/room-list.tsx
+++ src/app/room-list.tsx
@@ -1,18 +1,14 @@
 'use client'
 
 import React, { useEffect } from 'react'
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardImage, CardTitle } from '@/components/ui/card'
+import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
 import { useState } from 'react'
-import { Heart, Star } from 'lucide-react'
-import { Button } from '@/components/ui/button'
 import CustomPagination from '@/components/pagination/custom-pagination'
-import { convertUSDToVND } from '@/format/currency'
 import { useLocation } from '@/hooks/useLocation'
 import { useRoom } from '@/hooks/useRoom'
 import { Skeleton } from '@/components/ui/skeleton'
 import { useSwalAlert } from '@/hooks/useSwalAlert'
-import Link from 'next/link'
-import { ROUTES } from '@/constants/routes'
+import RoomCard from '@/app/(user)/rooms/room-card'
 
 const RoomList = () => {
   const [isFirstLoading, setIsFirstLoading] = useState(true) // Kiểm soát UI khi lần đầu load trang
@@ -100,91 +96,18 @@ const RoomList = () => {
       {/* Room List */}
       <div className='grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4'>
         {data?.content.data.map((room, index) => {
-          // Tạo mảng các tiện nghi dựa trên giá trị boolean
-          const amenities = [
-            room.wifi && 'Wifi',
-            room.bep && 'Bếp',
-            room.mayGiat && 'Máy giặt',
-            room.banLa && 'Bàn là',
-            room.tivi && 'Tivi',
-            room.dieuHoa && 'Điều hòa',
-            room.doXe && 'Đỗ xe',
-            room.hoBoi && 'Hồ bơi',
-            room.banUi && 'Bàn ủi'
-          ].filter(Boolean) // Lọc ra các giá trị 'true'
-
-          // Tạo mảng chứa thông tin phòng ngủ, giường, phòng tắm
-          const roomDetails = [
-            room.phongNgu > 0 && `${room.phongNgu} phòng ngủ`,
-            room.giuong > 0 && `${room.giuong} giường`,
-            room.phongTam > 0 && `${room.phongTam} phòng tắm`
-          ].filter(Boolean) // Lọc ra các giá trị hợp lệ
-
           // Tìm thông tin vị trí từ dataAllLocations dựa vào room.maViTri
           const location = dataAllLocations?.content.find((loc) => loc.id === room.maViTri)
 
           return (
-            <Link key={room.id} href={ROUTES.USER.ROOMS.ROOM_DETAIL(room.id.toString())}>
-              <Card className='group bg-background border-none shadow-none cursor-pointer hover:overflow-hidden'>
-                <div className='relative overflow-hidden rounded-xl'>
-                  <CardImage
-                    src={room.hinhAnh}
-                    alt={`image-${room.id}`}
-                    className='aspect-square object-cover object-center transition-transform duration-300 ease-in-out group-hover:scale-105'
-                  />
-                  {/* Heart Icon Button */}
-                  <Button
-                    variant='ghost'
-                    size='icon'
-                    className='absolute top-2 right-2 md:top-3 md:right-3 rounded-full transition-transform duration-300 hover:bg-transparent hover:scale-125'
-                    onClick={() => toggleWishlist(index)}
-                  >
-                    <Heart
-                      className={`w-5 h-5 md:w-7 md:h-7 text-white ${wishlist[index] ? 'fill-primary' : 'fill-black/50'}`}
-                    />
-                  </Button>
-                  {/* Guest Favorite */}
-                  {ratings[index] >= 4.5 && (
-                    <div className='absolute top-4 left-3 rounded-full bg-white px-2 py-1'>
-                      <p className='text-[9px] md:text-sm text-black font-semibold'>Được khách yêu thích</p>
-                    </div>
-                  )}
-                </div>
-                <CardHeader className='px-0 pt-4 pb-1 space-y-1'>
-                  <CardTitle className='flex justify-between gap-5'>
-                    <p className='truncate'>{room.tenPhong}</p>
-                    {/* Rating */}
-                    <p className='flex items-center'>
-                      <Star className='w-4 h-4 me-1 fill-current' />
-                      {ratings[index] >= 0 && ratings[index] <= 1 ? 'Mới' : ratings[index]}
-                    </p>
-                  </CardTitle>
-                  <CardDescription className='truncate'>
-                    {location ? (
-                      <>
-                        {location.tenViTri} | {location.tinhThanh} <br />
-                        {location.quocGia}
-                      </>
-                    ) : (
-                      <>
-                        {'Chưa cập nhật vị trí'} <br /> {'???'}
-                      </>
-                    )}
-                  </CardDescription>
-                </CardHeader>
-                <CardContent className='text-sm px-0 pb-3 text-muted-foreground'>
-                  <p className='truncate font-semibold'>
-                    {roomDetails.length > 0 ? roomDetails.join(' • ') : 'Chưa cập nhật thông tin phòng'}
-                  </p>
-                  <p className='truncate'>{amenities.length > 0 ? amenities.join(' • ') : 'Chưa cập nhật tiện nghi'}</p>
-                </CardContent>
-                <CardFooter className='px-0'>
-                  <p>
-                    <span className='font-semibold'>₫ {convertUSDToVND(room.giaTien)}</span> / đêm
-                  </p>
-                </CardFooter>
-              </Card>
-            </Link>
+            <RoomCard
+              key={room.id}
+              room={room}
+              location={location}
+              isInWishlist={wishlist[index]}
+              rating={ratings[index]}
+              onWishlistToggle={() => toggleWishlist(index)}
+            />
           )
         })}
       </div>
