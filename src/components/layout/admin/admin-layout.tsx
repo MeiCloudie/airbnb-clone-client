848e6ad51b40653d8969f339f1f5a191544e3293
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-10-07 18:55:04 +0700

    feat(admin): Update Loading admin page

--- src/components/layout/admin/admin-layout.tsx
+++ src/components/layout/admin/admin-layout.tsx
@@ -1,8 +1,10 @@
 'use client'
 
+import AirbnbLogo from '@/components/icon/airbnb-logo'
 import Header from '@/components/layout/admin/header'
 import Sidebar from '@/components/layout/admin/sidebar'
 import { useSession } from 'next-auth/react'
+import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 
@@ -26,7 +28,15 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
   }, [status, session, router])
 
   if (isLoading) {
-    return <p>Loading...</p>
+    return (
+      <div className='flex flex-col items-center justify-center text-center min-h-screen'>
+        <div className='w-96 h-auto'>
+          <Image src={'/logo/airbnb-logo-fullname.png'} alt='airbnb-logo-fullname' width={500} height={500} />
+        </div>
+        <h3 className='mt-4 text-lg md:text-xl font-semibold'>Chào mừng bạn đến với Hệ Thống Quản Trị</h3>
+        <p className='mt-2 text-gray-600'>Đang tải dữ liệu...</p>
+      </div>
+    )
   }
 
   return (
