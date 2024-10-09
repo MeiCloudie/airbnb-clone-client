de89993a47c21500b93219e19f7677fc93498635
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-10-10 01:02:59 +0700

    feat(admin): Update admin home page

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,9 +1,16 @@
 import PageContainer from '@/components/layout/page-container'
+import Image from 'next/image'
 
 export default function AdminHome() {
   return (
     <PageContainer scrollable={true}>
-      <h1>AdminHome Page</h1>
+      <div className='flex justify-center items-center h-[80vh]'>
+        <div className='w-96 h-auto text-center'>
+          <Image src={'/logo/airbnb-logo-fullname.png'} alt='airbnb-logo-fullname' width={500} height={500} />
+          <h3 className='mt-4 text-lg md:text-xl font-semibold'>Chào mừng Admin quay trở lại 👋</h3>
+          <p className='mt-2 text-gray-600'>Hiện tại chưa cập nhật Biểu Đồ</p>
+        </div>
+      </div>
     </PageContainer>
   )
 }
